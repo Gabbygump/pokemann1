8f25324b7fb9546eefabd7c2bad97ace26ce7ab8
Gabbygump <ggumpe6907@greenvilleschools.us>
2017-03-20 15:42:38 -0400

    Add files via upload

--- pokemann_starter.py
+++ pokemann_starter.py
@@ -1,7 +1,7 @@
 import random
 class Pokemann:
 
-    def __init__(self, name, kind, attack, defense, speed, health, moves, image):
+    def __init__(self, name, kind, attack, defense, speed, health, catch_rate, moves, image):
 
         self.name = name
         self.kind = kind
@@ -14,7 +14,7 @@ class Pokemann:
 
         self.fainted = False
         self.current_health = health
-
+        self.catch_rate = catch_rate
 
     def get_available_moves(self):
         result = []
@@ -51,41 +51,31 @@ class Pokemann:
             self.faint()
             
     def faint(self):
-        if self.current_health <= 0:
-            self.fainted = True
-            print(self.name + " fainted!")
+        self.current_health = 0
+        print(self.name + " fainted!")
+        self.fainted = True
                   
     def heal(self, amount):
         """
         Raises current_health by amount but not more than the base health.
         """
         self.current_health += amount
-        print(self.name + " now has " + str(self.current_health) +" health")
+        print(self.name + "'s health is restored")
         if self.current_health > self.health:
             self.current_health = self.health
-        self.fainted = False
-        
+            self.fainted = False
+        print(self.name +"'s health is now {}/{}.".format(self.current_health, self.health))
+
     def restore(self):
-        self.current_health = self.health
-        print("Power Restored")
-        self.remaining_power = self.powerpoint
-        """
-        Restores all health and resets powerpoint for all moves.
-        """
-        pass
+       self.heal(self.health)
+       moves = self.get_available_moves
+       for m in self.moves:
+           m.remaining_power = m.powerpoint
+           print("Move Restored")
         
 
     def draw(self):
         pass
-    
-    def get_available_moves(self):
-        result = []
-                  
-        for m in self.moves:
-            if m.remaining_power > 0:
-                  result.append(m)
-                  
-        return result
                   
     def get_move(self):
         """
@@ -133,34 +123,50 @@ class Move:
         
         return round(p * a / d * e) 
     
-    def restore(self):
-        move.remianing power
-        self.current_health = self.health
+    def restore(self, move):
+        move.remaining_power = move.powerpoint 
         print("Power Restored")
-
-        
         
         
 class Character:
     
-    def __init__(self, name, pokemann, image):
+    def __init__(self, name, party, image):
         self.name = name
-        self.pokemann = pokemann
+        self.party = party
         self.image = image
-
+            
     def get_available_pokemann(self):
         """
         Returns a list of all unfainted Pokemann belonging to a character.
         """
-        pass
-    
-    def get_first_pokemann(self):
+        result = []
+        for p in self.party:
+            if p.current_health == 0:
+                p.fainted = True
+            if not p.fainted:
+                result.append(p.name)
+
+        return result
+        
+
+    def restore(self):
+        for p in self.party:
+            p.restore()
+
+    def get_active_pokemann(self):
         """
-        Returns the first [0] unfainted character in the pokemann list.
+        Returns the first unfainted character in the pokemann list. If all pokemann
+        are fainted, return None.
         """
-        pass
+        available = self.get_available_pokemann()
+
+        if len(available) > 0:
+            return available[0]
+        else:
+            return None
+        
     
-    def set_first_pokemann(self, swap_pos):
+    def set_active_pokemann(self, swap_pos):
         """
         Moves pokemann to first position [0] in the pokemann list by exchanging it with
         pokemann located at swap_pos.
@@ -172,17 +178,61 @@ class Character:
 
 class Player(Character):
 
-    def __init__(self, name, pokemann):
-        Character.__init__(self, name, pokemann)
+    def __init__(self, name, party, image):
+        Character.__init__(self, name, party, image)
         
-        self.collection = []
+        self.computer = []
         self.pokeballs = 0
+        self.image = image
+        self.party = party
+    def catch(self, target):
+        """
+        Can only be applied to a wild pokemann. Determine a catch by generating a random
+        value and comparing it to the catch_rate. If a catch is successful, append the
+        target to the player's pokemann list. However, if the pokemann list already
+        contains 6 pokemann, add the caught target to the players computer instead.
+        Pokemann sent to the computer will be fully restored, but other caught pokemann
+        will remain at the strength they were caught. Decrease the player's pokeball
+        count by 1 regardless of success.
+        Return True if the catch is successful and False otherwise.
+        """
+        r = random.randint(1, 100)
+        if self.pokeballs != 0:
+            self.pokeballs -= 1
+            if r <= target.catch_rate:
+                if len(self.party) >= 6:
+                    self.computer.append(target)
+                    for n in self.computer:
+                        n.restore()
+                    print("You caught " + target.name + ".")
+                else:
+                    self.party.append(target)
+                    print("You caught " + target.name + ".")
+            else:
+                print("It got away")
+        else:
+            print("No Pokeballs Remaining")
+            
+    def run(self, target):
+        """
+        Can only be applied in the presence of a wild pokemann. Success is determined by
+        comparing speeds of the player's active pokemann and the wild pokemann. Incoroporate
+        randomness so that speed is not the only factor determining success.
+        Return True if the escape is successful and False otherwise.
+        """
+        corn = self.get_active_pokemann()
 
-class Opponent(Character):
+        r = random.randint(1,100)
+        if r <= 50:
+            return corn.speed >= target.speed
+        else:
+            return False
 
-    def __init__(self, name, pokemann):
-        Character.__init__(self, name, pokemann)
+class NPC(Character):
 
+    def __init__(self, name, party, image):
+        Character.__init__(self, name, party, image)
+        
 class Game:
 
     def __init__(self):
@@ -249,14 +299,6 @@ class Game:
         """
         pass
     
-    def catch(self, target):
-        """
-        Can only be applied to wild pokemann. Determine a catch by generating a random
-        value based on the target health. If a catch is successful, add the target to the
-        player's collection. Decrease the player's pokeball count by 1 regardless of success.
-        (Perhaps pokeballs kind could be incorporated into the probability at some point.)
-        """
-        pass
 
     def encounter(self, player, target):
         """
@@ -293,25 +335,43 @@ class Game:
 if __name__ == '__main__':
 
     # Make some moves
-    homework = Move("Homework", "teacher", 80, 10, 100)
-    pop_quiz = Move("Pop quiz", "teacher", 20, 60, 45)
-    lecture = Move("Lecture", "teacher", 50, 30, 60)
+    homework = Move("Homework", "teacher", 5, 10, 100)
+    pop_quiz = Move("Pop quiz", "teacher", 3, 30, 45)
+    lecture = Move("Lecture", "teacher", 1, 15, 60)
     teacher_strike = Move("Teacher Strike", "teacher", 20, 60, 45)
-    complaining = Move("Complaining", "teacher", 80, 10, 100)
 
-    phone_out = Move("Phone put during lesson", "student", 80, 10, 100)
-    no_hw = Move("No HW", "student", 50, 30, 60)
-    talking_back = Move("Talking Back", "student", 20, 60, 45)
-    complaining = Move("Complaining about social life", "student", 50, 30, 60)
+    phone_out = Move("Phone put during lesson", "student", 8, 10, 100)
+    no_hw = Move("No HW", "student", 5, 30, 60)
+    talking_back = Move("Talking Back", "student", 2, 60, 45)
+    complaining = Move("Complaining about social life", "student", 5, 30, 60)
 
-    ask_id = Move("Ask for ID", "administrator", 50, 10, 100)
-    call = Move("Call Parents", "administrator", 40, 40, 70)
-    expulsion = Move("Expulsion", "administrator", 10, 80, 35)
-    fired = Move("Fire Staff Member", "administrator", 10, 80, 35)
-    restrict_lunch = Move("Restrict Lunchtime", "administrator", 40, 40, 70)
+    ask_id = Move("Ask for ID", "administrator", 5, 10, 100)
+    call_parents = Move("Call Parents", "administrator", 4, 40, 70)
+    expulsion = Move("Expulsion", "administrator", 1, 80, 35)
+    fired = Move("Fire Staff Member", "administrator", 1, 80, 35)
+    restrict_lunch = Move("Restrict Lunchtime", "administrator", 4, 40, 70)
 
     # Create some Pokemann(s)
-    coopasaur = Pokemann("coopasaur", "teacher", 30, 20, 50, 30, [homework, pop_quiz, lecture], "coopasaur.png")
-    mayfieldarow = Pokemann("mayfieldarow", "administrator", 30, 20, 50, 30, [ask_id, fired, call], "mayfieldarow.png")
-    andrewag = Pokemann("andrewag", "student", 30, 20, 50, 30, [phone_out, no_hw, complaining], "andrewag.png")
+    coopasaur = Pokemann("Coopasaur", "teacher", 30, 20, 50, 100, 100, [homework, pop_quiz, ask_id], "coopasaur.png")
+    cookmander = Pokemann("Cookmander", "teacher", 30, 20, 50, 100, 100, [lecture, ask_id, homework], "cookmander.png")
+    vincolairy = Pokemann("Vincolairy", "teacher", 30, 20, 50, 120,100, [lecture, ask_id, homework], "vincolairy.png")
+    mayfieldarow = Pokemann("Mayfieldarow", "administrator", 30, 20, 50, 90, 100, [restrict_lunch, fired, expulsion], "mayfieldarow.png")
+    andrewag = Pokemann("Andrewag", "student", 30, 20, 50, 150, 100, [complaining, phone_out, no_hw], "andrewag.png")
+    caseypuff = Pokemann("Caseypuff", "student", 30, 20, 50, 170, 100, [complaining, phone_out, no_hw], "caseypuff.png")
+    colboreon = Pokemann("Colboreon", "student", 30, 20, 50, 80, 100, [complaining, phone_out, no_hw], "colboreon.png")
+    blakachu = Pokemann("Blakachu", "student", 30, 20, 50, 130, 100, [complaining, phone_out, no_hw], "blakachu.png")
+    zoeotto = Pokemann("Zoeotto", "student", 30, 20, 50, 100, 100, [complaining, phone_out, no_hw], "zoeotto.png")
+    morganyta = Pokemann("Morganyta", "student", 30, 20, 50, 160, 100,[complaining, phone_out, no_hw], "morganyta.png")
+    katlevee = Pokemann("Katlevee", "student", 30, 20, 50, 140, 100, [complaining, phone_out, no_hw], "katlevee.png")
+    marcelax = Pokemann("Marcelax", "student", 30, 20, 50, 30, 100, [complaining, phone_out, no_hw], "marcelax.png")
 
+
+    # Create Player
+    pat = Player("Pat Riotum", [coopasaur, andrewag, caseypuff, blakachu], "pat.png")
+
+    # Create Opponents
+    rocket = NPC("Team Rocket", [colboreon, zoeotto, morganyta, cookmander], "rocket.png")
+    jessie = NPC("Jessie", [vincolairy, mayfieldarow, katlevee, marcelax], "jessie.png")
+
+    # Create a game
+    g = Game()
